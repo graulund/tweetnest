7f25b9c313c60f0d263e1c17b42eb160233287a5
Victor Jacobs <victor.jacobs@me.com>
2013-07-02 09:48:57 +0200

    Cleaned up

--- inc/twitteroauth/config.php
+++ inc/twitteroauth/config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'roWBfb6P8ylWYEB0gkMNQ');
-define('CONSUMER_SECRET', 'wnuS7qdCqAV0q2OE7n0hAuP8TkEPwCiCbW4r554');
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
 define('OAUTH_CALLBACK', 'http://' . $_SERVER['SERVER_NAME'] . str_replace(basename($_SERVER['PHP_SELF']), "callback.php", $_SERVER['PHP_SELF']));
