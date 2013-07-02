b6d847dfe2c88b5eefd2f63e46aae1f4b47d4395
Andy Graulund <andreas@graulund.com>
2013-07-02 20:10:31 +0200

    Resolving bug in callback URL generation

--- inc/twitteroauth/config.php
+++ inc/twitteroauth/config.php
@@ -7,4 +7,6 @@
 
 define('CONSUMER_KEY', $config['consumer_key']);
 define('CONSUMER_SECRET', $config['consumer_secret']);
-define('OAUTH_CALLBACK', 'http://' . $_SERVER['SERVER_NAME'] . str_replace(basename($_SERVER['PHP_SELF']), "callback.php", $_SERVER['PHP_SELF']));
+
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? 'https://' : 'http://';
+define('OAUTH_CALLBACK', $protocol . $_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['PHP_SELF']), 'callback.php', $_SERVER['PHP_SELF']));
