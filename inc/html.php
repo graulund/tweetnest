aceb96bcba328591a81bf30a582420261f61b47d
Dominik Schwind <dschwind@lostfocus.de>
2013-07-04 22:35:02 +0200

    Catching a few PHP notices.

--- inc/html.php
+++ inc/html.php
@@ -120,7 +120,7 @@
 		if(!empty($tweet['place'])){
 			$tweetplace = unserialize(str_replace("O:16:\"SimpleXMLElement\"", "O:8:\"stdClass\"", $tweet['place']));
 		}
-		$rt = (array_key_exists("rt", $tweetextra) && !empty($tweetextra['rt']));
+		$rt = (is_array($tweetextra) && array_key_exists("rt", $tweetextra) && !empty($tweetextra['rt']));
 		$t  = str_repeat("\t", $tabs);
 		if($rt){ $retweet = $tweetextra['rt']; }
 		
